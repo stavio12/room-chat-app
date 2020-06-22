6d88ff63dc9293b19a574fe34b28eed71f60650f
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2020-06-22 05:31:25 +0000

    initial commit

--- functions/index.js
+++
@@ -1,7 +0,0 @@
-const admin = require("firebase-admin");
-
-admin.initializeApp();
-
-exports.userStatusChanged = require("./triggers/statusChange");
-exports.helloWorld = require("./routers/helloWorld");
-exports.chatBot = require("./triggers/chatBot");
--- functions/routers/helloWorld.js
+++
@@ -1,6 +0,0 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
